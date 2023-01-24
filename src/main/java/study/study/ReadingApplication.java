df635bf1009ba8e4d6ea1809077b21c267b1c54a
jhsong2580 <thdwmdgns@naver.com>
2023-01-24 17:47:02 +0900

    project init

---
+++ src/main/java/study/study/ReadingApplication.java
@@ -0,0 +1,13 @@
+package study.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/study/ReadingApplicationTests.java
@@ -0,0 +1,13 @@
+package study.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
